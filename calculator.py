fc82e8acacd059f0d9d3fb0aafbc353b6954cc78
badgersky <szymonek.ptasznik@gmail.com>
2023-06-16 14:12:13 +0200

    create buttons for numbers 0-9 and buttons for brackets

--- calculator.py
+++ calculator.py
@@ -11,7 +11,8 @@ class Calculator(ctk.CTk):
         ctk.set_appearance_mode('System')
 
         # configure grid
-        self.rowconfigure((0, 1), weight=1)
+        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=25)
         self.columnconfigure(0, weight=1)
 
         # text box for calculations
@@ -21,3 +22,28 @@ class Calculator(ctk.CTk):
         # frame for all buttons
         self.btn_frame = ctk.CTkFrame(self)
         self.btn_frame.grid(column=0, row=1, padx=10, pady=10, sticky='nsew')
+
+        self.btn_frame.rowconfigure((0, 1, 2, 3), weight=1)
+        self.btn_frame.columnconfigure((0, 1, 2), weight=1)
+        self.btn_frame.columnconfigure((3, 4), weight=2)
+
+        # buttons
+        # number buttons 
+        self.create_number_buttons()
+
+    def create_number_buttons(self):
+        button_num = 1
+        for row in range(4):
+            for col in range(3):
+                if row != 3:
+                    new_btn = ctk.CTkButton(self.btn_frame, height=30, width=30, text=str(button_num))
+                    button_num += 1
+                else:
+                    if col == 0:
+                        new_btn = ctk.CTkButton(self.btn_frame, height=30, width=30, text='(')
+                    if col == 1:
+                        new_btn = ctk.CTkButton(self.btn_frame, height=30, width=30, text='0')
+                    if col == 2:
+                        new_btn = ctk.CTkButton(self.btn_frame, height=30, width=30, text=')')
+                new_btn.grid(row=row, column=col, padx=2, pady=2)
+                
