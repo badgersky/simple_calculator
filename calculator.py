9e00f7cf9a8c87800ce39977da119c98e756b973
badgersky <szymonek.ptasznik@gmail.com>
2023-06-16 13:32:14 +0200

    add textbox for calculations

--- calculator.py
+++ calculator.py
@@ -9,3 +9,8 @@ class Calculator(ctk.CTk):
         self.geometry('400x400')
         self.title('Calculator')
         ctk.set_appearance_mode('System')
+
+        # text box for calculations
+        self.txt_calculations = ctk.CTkTextbox(self, height=120, width=380)
+        self.txt_calculations.grid(column=0, row=0, padx=10, pady=10)
+        
